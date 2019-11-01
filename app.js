46bd717618a2c57f68e27d31dd7521b90c2f73b8
kiranpuli <pulikiran070899@gmail.com>
2019-11-01 20:57:23 +0530

    order and rating

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-let rate=document.getElementById("rate");
-let ratebtn=document.getElementById("rate-btn");
+// let rate=document.getElementById("rate");
+// let ratebtn=document.getElementById("rate-btn");
 
-ratebtn.addEventListener("click",changeRate);
+// ratebtn.addEventListener("click",changeRate);
 
-function changeRate(){
-    let x = prompt('Rate the item');
-    rate.innerText=x;
-}
+// function changeRate(){
+//     let x = prompt('Rate the item');
+//     rate.innerText=x;
+// }
