89ffc67e0a96360ab9c9fcc24399b4a55dfb6e51
kiranpuli <pulikiran070899@gmail.com>
2019-11-03 17:34:06 +0530

    orderList page added

---
+++ orderList.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Foodie | Order-List</title>
+    <link rel="stylesheet" href="./bootstraplitera.min.css">
+  <link rel="stylesheet" href="./style1.css">
+</head>
+<body>
+        <div class="container">
+            <header class="bg-success  p-3">
+                <div class="row ">
+                  <div class="col-md-4 text-left "><a href="./homepage.php" class="btn btn-default text-white"><h3><i>Foodie</i></h3></a></div>
+                  <div class="col-md-4 text-center text-white border pt-3"><h5>Your Orders</h5></div>
+                  <div class="col-md-4 text-right"> <a class="btn btn-warning " href="index.php">Logout</a></div>
+                </div>
+          </header>
+                
+          <section class="hotel-list">
+                  <div class="row">
+                    <div class="col-sm-12">
+                      <div class="card ">
+                        <div class="card-body border-success shadow m-2 row">
+                          <div class="col-md-8">
+                            <h3 class="card-title">Order-ID</h3>
+                          </div>
+
+                          <div class="col-md-4 text-right ">
+                            <p class=" text-center lead">test</p>
+                            <p class=" text-center lead">test</p>
+                            <p class=" text-center lead">test</p>
+                            <p class=" text-center lead">test</p>
+                            <hr>
+                            <p class="card-text lead text-center">Total Bill</p>
+                            <hr>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    
+                </section>
+
+            
+                <footer class="bg-dark text-white p-3">
+                    <div class="row">
+                      <div class="col-md-6 text-left"><a href="./aboutpage.html" class="btn btn-success">About Us</a></div>
+                      <div class="col-md-6 text-right"> &#9400 Copyright , 2019.</div>
+                    </div>
+                  </footer>
+              </div>
+</body>
+</html>
